97a5af22abfd9170f273a651bded0e712d5dfca1
Danielle H <daheber@gmail.com>
2017-10-29 13:57:18 -0700

    translate intent repeats user input

--- pittsburghese_translator/lambda/custom/index.js
+++ pittsburghese_translator/lambda/custom/index.js
@@ -1,43 +1,47 @@
 'use strict';
 var Alexa = require("alexa-sdk");
 
-// For detailed tutorial on how to making a Alexa skill,
-// please visit us at http://alexa.design/build
-
-
 exports.handler = function(event, context) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('MainMenu');
-    },
-    'MainMenuIntent': function () {
-        this.emit('MainMenu');
-    },
-    'MainMenu': function () {
-        this.response.speak('At the main menu');
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest' : function() {
-        console.log('Session ended with reason: ' + this.event.request.reason);
-    },
-    'AMAZON.StopIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'enter help intent directions here'");
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't get that. You can try: 'insert unhandled info here'");
-    }
+  'LaunchRequest': function () {
+    this.emit('MainMenu');
+  },
+  'MainMenuIntent': function () {
+    this.emit('MainMenu');
+  },
+  'TranslateIntent': function () {
+    this.emit('Translate');
+  },
+  'MainMenu': function () {
+    this.response.speak('you are at the main menu');
+    this.emit(':responseReady');
+  },
+  'Translate': function() {
+    var phraseToTranslate = this.event.request.intent.slots.Phrase.value;
+    this.response.speak('In the translate:' + phraseToTranslate);
+    this.emit(':responseReady');
+  },
+  'SessionEndedRequest' : function() {
+    console.log('Session ended with reason: ' + this.event.request.reason);
+  },
+  'AMAZON.StopIntent' : function() {
+    this.response.speak('Bye');
+    this.emit(':responseReady');
+  },
+  'AMAZON.HelpIntent' : function() {
+    this.response.speak("You can try: 'enter help intent directions here'");
+    this.emit(':responseReady');
+  },
+  'AMAZON.CancelIntent' : function() {
+    this.response.speak('Bye');
+    this.emit(':responseReady');
+  },
+  'Unhandled' : function() {
+    this.response.speak("Sorry, I didn't get that. You can try: 'insert unhandled info here'");
+  }
 };
