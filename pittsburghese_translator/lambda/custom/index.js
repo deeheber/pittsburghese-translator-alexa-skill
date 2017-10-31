154e0066dd039ccbdaec6d1138f235b66cb8b460
Danielle H <daheber@gmail.com>
2017-10-30 17:36:58 -0700

    minor cleanup

--- pittsburghese_translator/lambda/custom/index.js
+++ pittsburghese_translator/lambda/custom/index.js
@@ -9,16 +9,16 @@ exports.handler = function(event, context) {
 };
 
 var handlers = {
-  'LaunchRequest': function () {
+  'LaunchRequest': function() {
     this.emit('MainMenu');
   },
-  'MainMenuIntent': function () {
+  'MainMenuIntent': function() {
     this.emit('MainMenu');
   },
-  'TranslateIntent': function () {
+  'TranslateIntent': function() {
     this.emit('Translate');
   },
-  'MainMenu': function () {
+  'MainMenu': function() {
     this.emit(':ask', 'Welcome to the Pittsburghese translator. Say translate and the phrase you would like to translate.');
   },
   'Translate': function() {
@@ -27,24 +27,24 @@ var handlers = {
     this.response.speak('In the translate:' + phraseToTranslate);
     this.emit(':responseReady');
   },
-  'SessionEndedRequest' : function() {
-    console.log('Session ended with reason: ' + this.event.request.reason);
-  },
-  'AMAZON.StopIntent' : function() {
+  'AMAZON.StopIntent': function() {
     this.response.speak('Bye');
     this.emit(':responseReady');
   },
-  'AMAZON.HelpIntent' : function() {
+  'AMAZON.HelpIntent': function() {
     // TODO add actual directions
     this.response.speak("You can try: 'enter help intent directions here'");
     this.emit(':responseReady');
   },
-  'AMAZON.CancelIntent' : function() {
+  'AMAZON.CancelIntent': function() {
     this.response.speak('Bye');
     this.emit(':responseReady');
   },
-  'Unhandled' : function() {
+  'Unhandled': function() {
     // TODO add helpful speech here
     this.response.speak("Sorry, I didn't get that. You can try: 'insert unhandled info here'");
+  },
+  'SessionEndedRequest': function() {
+    console.log('Session ended with reason: ' + this.event.request.reason);
   }
 };
