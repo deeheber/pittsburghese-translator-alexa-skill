d27c617ae516b5d507cc7621580bbad7eedc0a0c
Danielle H <daheber@gmail.com>
2017-10-31 18:07:29 -0700

    update messages in intent replies

--- pittsburghese_translator/lambda/custom/index.js
+++ pittsburghese_translator/lambda/custom/index.js
@@ -15,34 +15,33 @@ var handlers = {
   'MainMenuIntent': function() {
     this.emit('MainMenu');
   },
-  'TranslateIntent': function() {
-    this.emit('Translate');
-  },
   'MainMenu': function() {
     this.emit(':ask', 'Welcome to the Pittsburghese translator. Say translate and the phrase you would like to translate.');
+  },
+  'TranslateIntent': function() {
+    this.emit('Translate');
   },
   'Translate': function() {
     var phraseToTranslate = this.event.request.intent.slots.Phrase.value;
     // TODO transform the output to pittsburghese here probably by using a dictionary
-    this.response.speak('In the translate:' + phraseToTranslate);
-    this.emit(':responseReady');
-  },
-  'AMAZON.StopIntent': function() {
-    this.response.speak('Bye');
+    this.response.speak(phraseToTranslate);
     this.emit(':responseReady');
   },
   'AMAZON.HelpIntent': function() {
-    // TODO add actual directions
-    this.response.speak("You can try: 'enter help intent directions here'");
-    this.emit(':responseReady');
+    this.emit('Say translate and the phrase you would like to translate. For example you can try, "translate" I am going downtown');
+  },
+  'AMAZON.StopIntent': function() {
+    this.emit('Bye');
   },
   'AMAZON.CancelIntent': function() {
-    this.response.speak('Bye');
+    this.emit('Bye');
+  },
+  'Bye': function() {
+    this.response.speak('Thanks for stopping by. Catch you next time.');
     this.emit(':responseReady');
   },
   'Unhandled': function() {
-    // TODO add helpful speech here
-    this.response.speak("Sorry, I didn't get that. You can try: 'insert unhandled info here'");
+    this.emit(':ask', "Sorry, I didn't get that. You can say, 'translate' and the phrase you would like to hear");
   },
   'SessionEndedRequest': function() {
     console.log('Session ended with reason: ' + this.event.request.reason);
