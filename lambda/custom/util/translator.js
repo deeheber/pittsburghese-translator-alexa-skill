de543c4c25772d032a305936839abd6386f9e31d
Danielle H <daheber@gmail.com>
2017-11-13 12:51:00 -0800

    base work on translate functionality complete

--- lambda/custom/util/translator.js
+++ lambda/custom/util/translator.js
@@ -1,12 +1,23 @@
 module.exports = (dictionary, phrase) => {
   const words = phrase.split(' ');
+  const result = [];
 
-  return words
-    .map(word => {
-      if (dictionary[word]) {
-        return dictionary[word];
-      }
-      return word;
-    })
-    .join(' ');
+  for (let i = 0; i < words.length; i++) {
+    const currentWord = words[i];
+    const nextWord = words[i + 1];
+
+    if (dictionary[`${currentWord}_${nextWord}`]) {
+      // two word phrase match
+      result.push(dictionary[`${currentWord}_${nextWord}`]);
+      i++;
+    } else if (dictionary[currentWord]) {
+      // single word phrase match
+      result.push(dictionary[currentWord]);
+    } else {
+      // no word match, so return the word as is
+      result.push(words[i]);
+    }
+  }
+
+  return result.join(' ');
 }
