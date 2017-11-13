1cd7b08d3f6cce4009b492b4871124672729ad7a
Danielle H <daheber@gmail.com>
2017-11-13 12:56:46 -0800

    es6+ - ify a bit more

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -11,42 +11,42 @@ exports.handler = function(event, context) {
 };
 
 const handlers = {
-  'LaunchRequest': function() {
+  'LaunchRequest'() {
     this.emit('MainMenu');
   },
-  'MainMenuIntent': function() {
+  'MainMenuIntent'() {
     this.emit('MainMenu');
   },
-  'MainMenu': function() {
+  'MainMenu'() {
     this.emit(':ask', 'Welcome to the Pittsburghese translator. Say translate and the phrase you would like to translate.');
   },
-  'TranslateIntent': function() {
+  'TranslateIntent'() {
     this.emit('Translate');
   },
-  'Translate': function() {
+  'Translate'() {
     const phraseToTranslate = this.event.request.intent.slots.Phrase.value;
     const translated = translator(dictionary, phraseToTranslate);
-    
+
     this.response.speak(translated);
     this.emit(':responseReady');
   },
-  'AMAZON.HelpIntent': function() {
+  'AMAZON.HelpIntent'() {
     this.emit('Say translate and the phrase you would like to translate. For example you can try, "translate" I am going downtown');
   },
-  'AMAZON.StopIntent': function() {
+  'AMAZON.StopIntent'() {
     this.emit('Bye');
   },
-  'AMAZON.CancelIntent': function() {
+  'AMAZON.CancelIntent'() {
     this.emit('Bye');
   },
-  'Bye': function() {
+  'Bye'() {
     this.response.speak('Thanks for stopping by. Catch you next time.');
     this.emit(':responseReady');
   },
-  'Unhandled': function() {
+  'Unhandled'() {
     this.emit(':ask', "Sorry, I didn't get that. You can say, 'translate' and the phrase you would like to hear");
   },
-  'SessionEndedRequest': function() {
+  'SessionEndedRequest'() {
     console.log('Session ended with reason: ' + this.event.request.reason);
   }
 };
